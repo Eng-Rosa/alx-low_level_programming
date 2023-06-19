27d98221c79551d7a8b03080f2a1bb2451aaa34a
Eng-Rosa <nesreen.gameel.89@gmail.com>
2023-06-19 16:39:45 -0700

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'write alpha'
+ * Return: always 0
+*/
+int main(void)
+{
+
+	int n;
+
+	for (n = 75; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
